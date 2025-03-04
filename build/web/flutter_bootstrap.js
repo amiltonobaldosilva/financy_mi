65b1fa06bf246f13c9eb1af259a7ee935118502b
Amilton Obaldo da Silva <mercadoimenso@gmail.com>
2025-03-03 22:26:34 -0300

    Titulo!

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "670536068"
+    serviceWorkerVersion: "3469973496"
   }
 });
